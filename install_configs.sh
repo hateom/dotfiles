3686c6d31b22088660cdc97a988c0b5009adec9c
Tomasz Huczek <hateom@gmail.com>
2017-06-18 14:50:42 +0200

    Added installation script
    Signed-off-by: Tomasz Huczek <hateom@gmail.com>

---
+++ install_configs.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.vimrc.local
+rm -rf ~/.vimrc.bundles.local
+rm -rf ~/.tmux.conf
+ln -s /Users/hateom/Workspace/dotfiles/.vimrc.local ~/.vimrc.local
+ln -s /Users/hateom/Workspace/dotfiles/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s /Users/hateom/Workspace/dotfiles/.tmux.conf ~/.tmux.conf
