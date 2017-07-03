f53a6a9975504c6d529269f5de20387877410892
tom@bmp.pl <tom@bmp.pl>
2017-07-03 20:11:27 +0200

    Some tmux and vim bundle changes

--- install_configs.sh
+++ install_configs.sh
@@ -1,6 +1,8 @@
 rm -rf ~/.vimrc.local
 rm -rf ~/.vimrc.bundles.local
 rm -rf ~/.tmux.conf
+rm -rf ~/.tmux
 ln -s /Users/hateom/Workspace/dotfiles/.vimrc.local ~/.vimrc.local
 ln -s /Users/hateom/Workspace/dotfiles/.vimrc.bundles.local ~/.vimrc.bundles.local
 ln -s /Users/hateom/Workspace/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s /Users/hateom/Workspace/dotfiles/.git ~/.git
